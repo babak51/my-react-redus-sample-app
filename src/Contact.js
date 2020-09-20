bcbdc757a606970858374ec3aee5cf7128dad733
Mohammad Amiraslani <baamira@yahoo.com>
2020-09-19 22:23:47 -0700

    connecting the dataStore tp props

--- src/Contact.js
+++ src/Contact.js
@@ -3,6 +3,7 @@ import './Contact.css';
 import { FaTrash } from 'react-icons/fa';
 export default class Contact extends Component {
     render() {
+      console.log("[Contact.js] >>>>---> props:",this.props);
       return <tr>
                  <td className="Contact-td" style={{width: "36px"}}>{this.props.contact.id}</td>
                  <td className="Contact-td" style={{width: "260"}}>{this.props.contact.name}</td>
@@ -11,7 +12,7 @@ export default class Contact extends Component {
                  <td className="Contact-td" style={{textAlign: "center"}}>
                        <button className="Contact-button" 
                                style={{borderStyle: "none"}}
-                               onClick={()=>this.props.deleteContact(this.props.contact.id)}>
+                               onClick={this.props.deleteContact}>
                           <FaTrash size={30}/>
                        </button>
                 </td>
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,22 @@
-import { createStore } from "redux";
+import { createStore} from "redux";
 import modelReducer from "./modelReducer";
 
 export default createStore(modelReducer);
 
 export { deleteContact } from "./modelActionCreator";
+
+
+/*
+import { createStore, combineReducers } from "redux";
+import modelReducer from "./modelReducer";
+import stateReducer from "./stateReducer";
+
+export default createStore(combineReducers(
+    {
+        modelData: modelReducer, 
+        stateData: stateReducer
+    }));
+
+export { saveProduct, saveSupplier, deleteProduct, deleteSupplier } 
+    from "./modelActionCreators";
+*/
